4f4b6b1822812c61d137d74ddd82aef693e14c30
MIGNAMENOU Kossi Jean-Marie <166952649+MJMWALTER20@users.noreply.github.com>
2025-12-06 17:07:46 +0000

    Update UserRepository.java

--- backend/src/main/java/com/agrismart/app/repositories/UserRepository.java
+++ backend/src/main/java/com/agrismart/app/repositories/UserRepository.java
@@ -1,1 +1,11 @@
+package com.agrismart.app.repositories;
+
+import com.agrismart.app.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByPhone(String phone);
+}
 
