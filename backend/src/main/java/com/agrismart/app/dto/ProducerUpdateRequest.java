ab3ff4af6cc802ac9ee43d0492f0e2787a5af74b
MIGNAMENOU Kossi Jean-Marie <166952649+MJMWALTER20@users.noreply.github.com>
2025-12-07 07:19:14 +0000

    Update ProducerUpdateRequest.java

--- backend/src/main/java/com/agrismart/app/dto/ProducerUpdateRequest.java
+++ backend/src/main/java/com/agrismart/app/dto/ProducerUpdateRequest.java
@@ -1,1 +1,13 @@
+package com.agrismart.app.dto;
 
+import lombok.Data;
+
+@Data
+public class ProducerUpdateRequest {
+    private String region;
+    private String village;
+    private Double gpsLatitude;
+    private Double gpsLongitude;
+    private Float landArea;
+    private String mainCrops;
+}
